9dbedbf9d9a163b0f2400557f3a18a84c8f75539
Kenta11 <kk.068.kenta@gmail.com>
2019-02-11 23:32:49 +0900

    first commit

---
+++ settings.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+#! -*- coding: utf-8 -*-
+
+export PATH=$PATH:$(cd $(dirname $0); pwd)
---
+++ template/tcl/cosim.tcl
@@ -0,0 +1,4 @@
+open_project  $env(HLS_TARGET)
+open_solution $env(HLS_SOLUTION)
+
+cosim_design
