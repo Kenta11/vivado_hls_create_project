c7043816ae4a3bac23b241b1a869b9560afc923e
Kenta11 <kk.068.kenta@gmail.com>
2019-02-19 23:42:26 +0900

    add function of C++ code generation

---
+++ template/c++/test-code.cpp
@@ -0,0 +1,14 @@
+#include "test_{{target}}.hpp"
+
+int main(int argc, char const **argv){
+    
+
+    return 0;
+}
+
+void
+test_{{target}}(
+    
+){
+    
+}
---
+++ template/c++/test-header.hpp
@@ -0,0 +1,6 @@
+#ifndef _TEST_{{TARGET}}_HPP_
+#define _TEST_{{TARGET}}_HPP_
+
+void test_{{target}}();
+
+#endif  //_TEST_{{TARGET}}_HPP_ 
